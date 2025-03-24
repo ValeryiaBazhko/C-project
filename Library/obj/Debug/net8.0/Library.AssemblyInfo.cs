f349380dd7d6fa2ef803bf40966968f0a4ad8f88
vbazhko <valeryia.bazhko@dinitcs.com>
2025-03-24 14:56:21 +0100

    modified web.config

--- Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
+++ Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43717381bfc464cd6d0408b404ab05600035baec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5870945c5027827cb8ed0bf0b4c838e72c9b5844")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
