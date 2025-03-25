931456e449efb22513c5d55828944a0d4d0d382e
vbazhko <valeryia.bazhko@dinitcs.com>
2025-03-25 08:57:54 +0100

    right ports for Library.dll

--- Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
+++ Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5870945c5027827cb8ed0bf0b4c838e72c9b5844")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+436f03028c38f4580663f12877d164d829a0b960")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
