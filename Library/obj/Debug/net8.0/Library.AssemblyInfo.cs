4c800e7f76f58955745fba1e0422fde648d104ff
vbazhko <valeryia.bazhko@dinitcs.com>
2025-03-25 15:42:24 +0100

    .

--- Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
+++ Library/obj/Debug/net8.0/Library.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfd640a2fbb9a94cb9faba53e795cd620da74c55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2379ab422df48ff213e09b6c2df6c2738648df1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
