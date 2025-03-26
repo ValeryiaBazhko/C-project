610835861fda82b4718bbbc6c5ad42a5b205ec7f
vbazhko <valeryia.bazhko@dinitcs.com>
2025-03-26 08:40:06 +0100

    workflow

--- TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c800e7f76f58955745fba1e0422fde648d104ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3437b477cf5ad93ad1b1413f8a565b50a5e6a283")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
