a741a474b5759c4306eec815d409757cce50b054
vbazhko <valeryia.bazhko@dinitcs.com>
2025-03-26 09:09:48 +0100

    .

--- Library/obj/Release/net8.0/Library.AssemblyInfo.cs
+++ Library/obj/Release/net8.0/Library.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f361169f2515e978d31298a0ab8e4b4fe9f34eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30934b4cc3d351c7453efccc81749831d7c9893d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
