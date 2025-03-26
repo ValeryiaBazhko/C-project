25ee5b316adb879c84e74fdbf5a0aff7a989a197
vbazhko <valeryia.bazhko@dinitcs.com>
2025-03-26 10:30:24 +0100

    .

--- TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95374d43d570a651701bd5797047dd2facabc8b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70e203a8a0bdbe4cdb6af7a3f6f539eb1d616026")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
